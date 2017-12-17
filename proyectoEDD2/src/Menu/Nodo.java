f6f9903f66dd0dfce99ebb177acfa57ca5cfd245
David Leonel Nuñez Elvir <davidelvir2007@hotmail.com>
2017-12-17 14:32:52 -0600

    holis

---
+++ proyectoEDD2/src/Menu/Nodo.java
@@ -0,0 +1,17 @@
+package Menu;
+
+public class Nodo{
+    
+    public int []valores;
+    public Nodo []nodo;
+    //public Registro [] registros;
+    public static int numValores;
+    public boolean tengoHijos = false;
+    public int ocupados = 0;
+    public Nodo padre;
+    public Nodo(){
+       nodo = new Nodo [Raiz.grado * 2 + 3];
+       valores = new int [Raiz.grado * 2 + 1];
+    }
+    
+}
